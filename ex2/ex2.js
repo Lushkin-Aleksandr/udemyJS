dca9285ac11bde50f3a6bab31e2e370c0cade507
Aleksandr <sanyalu97@gmail.com>
2020-01-29 20:01:25 +0300

    Задание 3 выполнено

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -23,10 +23,43 @@ for (let i = 0; i < 2; i++) {
         console.log("done");
         appData.expenses[a] = b;
     } else {
-
+        console.log("bad");
+        i--;
     }
     
 }
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце");
+//     let b = prompt("Во сколько обойдется?");
+
+//     if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
+//         && a !=  '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     } else {
+
+//     }
+
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце");
+//     let b = prompt("Во сколько обойдется?");
+
+//     if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
+//         && a !=  '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     } else {
+
+//     }
+
+//     i++;
+// } while (i < 2);
 
 appData.moneyPerDay = appData.budget / 30;
 
